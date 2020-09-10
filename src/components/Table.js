b2e8eb000ae423f00426c0828515f8d5eedaf1f4
Andrew <andrewgraemebrooks@protonmail.com>
2020-09-10 18:53:48 +0200

    The table now shows the history of searches

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,41 +1,29 @@
 import React, { Component } from "react";
-import { Map } from "immutable";
 
 class Table extends Component {
   render() {
-    // Gets the latest results. If there aren't any, default to empty Map
-    const latestResults = this.props.history.last(Map());
 
     return (
       <div className="col">
-        <table className="table table-bordered table-responsive">
+        <table className="table table-bordered">
           <thead>
             <tr>
               <th>Ingredient</th>
               <th>Calories</th>
-              <th>Cautions</th>
-              <th>Diet Labels</th>
-              <th>Health Labels</th>
-              <th>Ingredients</th>
-              <th>Total Daily</th>
               <th>Total Nutrients</th>
-              <th>Total Nutrients (KCal)</th>
-              <th>Total Weight</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>Example</th>
-              <th>{latestResults.get("calories")}</th>
-              <th>Example</th>
-              <th>Example</th>
-              <th>Example</th>
-              <th>Example</th>
-              <th>Example</th>
-              <th>Example</th>
-              <th>Example</th>
-              <th>Example</th>
-            </tr>
+            {this.props.history &&
+              this.props.history.map((result) => {
+                return (
+                  <tr>
+                    <td>{result.get("calories")}</td>
+                    <td>{result.get("calories")}</td>
+                    <td>{result.get("calories")}</td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
