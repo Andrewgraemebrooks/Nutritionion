05cbce3bb96d12ff740949432397dacfcb4ab218
Andrew <andrewgraemebrooks@protonmail.com>
2020-09-11 12:08:37 +0200

    The application now successfully parses the result's nested objects and displays their information. I also created a function to capitalise all the displayed words.

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 class Table extends Component {
+  /**
+   * Capitalises the first letter of each word in a sentence
+   * @param {string} string - The sentence to capitalise.
+   * @returns {string} Capitalised sentence
+   */
+  capitaliseSentence(string) {
+    return string
+      .split(" ")
+      .map((x) => x.charAt(0).toUpperCase() + x.slice(1))
+      .join(" ");
+  }
+
   render() {
     const history = this.props.history;
     return (
@@ -11,7 +23,7 @@ class Table extends Component {
             <tr>
               <th>Ingredient</th>
               <th>Calories</th>
-              <th>Total Nutrients</th>
+              <th>Health Labels</th>
             </tr>
           </thead>
           <tbody>
@@ -19,9 +31,31 @@ class Table extends Component {
               history.map((result) => {
                 return (
                   <tr key={uuidv4()}>
+                    <td>
+                      {
+                        // Retrieves the ingredient's information from the ingredient object.
+                        result.get("ingredients") &&
+                          result.get("ingredients").map((ingredient) => {
+                            return this.capitaliseSentence(ingredient.text);
+                          })
+                      }
+                    </td>
                     <td>{result.get("calories")}</td>
-                    <td>{result.get("calories")}</td>
-                    <td>{result.get("calories")}</td>
+                    <td>
+                      {
+                        // Retrieves the healthLabel information from the healthLabel object.
+                        result.get("healthLabels") &&
+                          result.get("healthLabels").map((healthLabel) => {
+                            return (
+                              <li key={uuidv4()}>
+                                {this.capitaliseSentence(
+                                  healthLabel.toLowerCase().replaceAll("_", " ")
+                                )}
+                              </li>
+                            );
+                          })
+                      }
+                    </td>
                   </tr>
                 );
               })}
