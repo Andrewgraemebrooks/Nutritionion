17334a64d75ac5a56eb7233718301224f1f11756
Andrew <andrewgraemebrooks@protonmail.com>
2020-11-11 13:18:30 +0000

    Update api.php
    Problem(s):
    * The API routes are not documented. This can lead to confusion for future developers.
    
    Solution(s):
    * Added PHPDoc documentation for the routes and added some comments to the routes.
    
    Notes:
    * Merge after 6pm EST

--- routes/api.php
+++ routes/api.php
@@ -19,19 +19,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * A test route to see if the nutrition API is working.
+ */
 Route::get('/nutrition/test', function () {
     return response()->json([
         'success' => 'true',
     ]);
 });
 
+/**
+ * The route that accepts a request for nutritional information and returns the data from the Edamam API.
+ */
 Route::post('/nutrition', function (Request $request) {
+    // GuzzleHttp Client to send a GET request to the Edamam API.
     $client = new Client();
+    // The user's input.
     $input = $request->input('userInput');
     $app_id = config('services.edamam.id');
     $app_key = config('services.edamam.key');
     $res = $client->get(
         'https://api.edamam.com/api/nutrition-data?app_id=' . $app_id . '&app_key=' . $app_key . '&ingr=' . $input
     );
+    // Return the response as a json.
     return response()->json(json_decode($res->getBody()));
 });
